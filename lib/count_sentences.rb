102e9325b97ef4983eab76eac047da00244ccbf5
Ryan Daley <rdaley321@yahoo.com>
2018-11-25 02:05:41 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,10 +15,6 @@ class String
   end
 
   def count_sentences
-    total = 0
-    total += self.split(".").count
-    total += self.split("!").count
-    total += self.split("?").count
-    total
+    self.split(/\.|\?|\!/).count
   end
 end
